81cac997b282f52f174b99a042183668af3fb466
David Freeman <davidfreeman@davids-mbp.lan>
2017-10-09 17:23:35 -0500

    added camera functionality

--- Tick Alert/Tick Alert/CameraViewController.swift
+++ Tick Alert/Tick Alert/CameraViewController.swift
@@ -7,13 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
-
+    @IBOutlet weak var capturePhotoButton: UIButton!
+    @IBOutlet weak var previewView: UIView!
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    
+    @IBAction func onTapCapture(_ sender: Any) {
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        
+        let photoSettings = AVCapturePhotoSettings()
+        
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .auto
+        
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.bringSubview(toFront: capturePhotoButton)
 
-        // Do any additional setup after loading the view.
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+            
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
+            
+            captureSession?.addOutput(capturePhotoOutput!)
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +71,25 @@ class CameraViewController: UIViewController {
     }
     */
 
+}
+
+extension CameraViewController : AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        guard error == nil,
+            let photoSampleBuffer = photoSampleBuffer else {
+                print("Error capturing photo: \(String(describing: error))")
+                return
+        }
+        
+        guard let imageData =
+            AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+                return
+        }
+        // Initialise a UIImage with our image data
+        let capturedImage = UIImage.init(data: imageData , scale: 1.0)
+        if let image = capturedImage {
+            // Save our captured image to photos album
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
 }
