2cbdbe3d02f3c258b496d2c50b72344833c06d11
David Freeman <d526f861@gmail.com>
2018-01-30 15:24:15 -0600

    finished google sign in and user account creation

--- Tick Alert/Tick Alert/AppDelegate.swift
+++ Tick Alert/Tick Alert/AppDelegate.swift
@@ -28,9 +28,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         }
         
         guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
-        // ...
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            // User is signed in
+            // ...
+        }
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
--- Tick Alert/Tick Alert/ProfileViewController.swift
+++ Tick Alert/Tick Alert/ProfileViewController.swift
@@ -24,22 +24,23 @@ class ProfileViewController: UIViewController, GIDSignInUIDelegate {
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
         
+        
         // TODO(developer) Configure the sign-in button look/feel
         // ...
 
-        ref = Database.database().reference()
-        self.ref.child("user/\(userId)/username").setValue("greasyw00t")
-        
-        
-        ref.child("user").child("\(userId)").observeSingleEvent(of: .value, with: { (snapshot) in
-            // Get user value
-            let value = snapshot.value as? NSDictionary
-            print(value)
-            self.userFirstAndLast.text = value?.value(forKey: "name") as! String
-            self.userLocation.text = value?.value(forKey: "location") as! String
-        }) { (error) in
-            print(error.localizedDescription)
-        }
+//        ref = Database.database().reference()
+//        self.ref.child("user/\(userId)/username").setValue("jellyw00t")
+//
+//
+//        ref.child("user").child("\(userId)").observeSingleEvent(of: .value, with: { (snapshot) in
+//            // Get user value
+//            let value = snapshot.value as? NSDictionary
+//            print(value)
+//            self.userFirstAndLast.text = value?.value(forKey: "name") as! String
+//            self.userLocation.text = value?.value(forKey: "location") as! String
+//        }) { (error) in
+//            print(error.localizedDescription)
+//        }
     }
 
     override func didReceiveMemoryWarning() {
