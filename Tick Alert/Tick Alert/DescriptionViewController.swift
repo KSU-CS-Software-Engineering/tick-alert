44f03fc8da4f040a9d1cd4eff12ac2ada5b015f7
David Freeman <d526f861@gmail.com>
2018-03-01 10:26:15 -0600

    added keyboard dismiss in description view

--- Tick Alert/Tick Alert/DescriptionViewController.swift
+++ Tick Alert/Tick Alert/DescriptionViewController.swift
@@ -25,6 +25,10 @@ class DescriptionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
     /*
     // MARK: - Navigation
